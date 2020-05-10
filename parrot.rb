d750504ea6d09569eb8c40a528fb3dd487621128
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-10 07:13:46 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(noise="Squawk!")
- puts "#{noise}"
+  puts "#{noise}"
+   "#{noise}"
 end
 
 parrot("Pretty Bird!")
